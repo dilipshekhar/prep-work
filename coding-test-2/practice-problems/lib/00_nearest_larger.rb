6d91e69327666b14044e61107c7355f5a8a20094
dilipshekhar <shekhar.dilip@gmail.com>
2014-05-26 15:37:23 -0400

    Update 00_nearest_larger.rb

--- coding-test-2/practice-problems/lib/00_nearest_larger.rb
+++ coding-test-2/practice-problems/lib/00_nearest_larger.rb
@@ -5,7 +5,7 @@ def nearest_larger(arr, idx)
     r = idx + dist  #right side of index
     
     return l if l >= 0 && arr[l] > arr[idx]
-    return r if r > (arr.count + 1) && arr[r] > arr[idx]
+    return r if r < (arr.count + 1) && arr[r] > arr[idx]
     return nil if l < 0 && r > arr.count + 1
     dist += 1
 end
