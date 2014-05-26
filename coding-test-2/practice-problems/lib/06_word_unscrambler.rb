4575c357295bb5298c3ac613a8911a820f522910
dilipshekhar <shekhar.dilip@gmail.com>
2014-05-26 15:56:45 -0400

    Update 06_word_unscrambler.rb

--- coding-test-2/practice-problems/lib/06_word_unscrambler.rb
+++ coding-test-2/practice-problems/lib/06_word_unscrambler.rb
@@ -1,2 +1,10 @@
-def word_unscrambler(str, words)
+def word_unscramble(word, dictionary)
+matched_words = []
+sorted_word = word.split("").sort
+dictionary.each do |i|
+	if i.split("").sort == sorted_word
+		matched_words << i
+	end
+end
+matched_words
 end
