9d4d94099000c4c673bfe53b084d3215e2b72f5c
dilipshekhar <shekhar.dilip@gmail.com>
2014-05-26 14:17:43 -0400

    Update 01_no_repeats.rb

--- coding-test-2/practice-problems/lib/01_no_repeats.rb
+++ coding-test-2/practice-problems/lib/01_no_repeats.rb
@@ -1,2 +1,18 @@
+def repeat_year?(year)
+  year_arr = year.split("")
+  seen = []
+  year.each do |i|
+    
+    return false if seen.include?
+    seen << i
+  end
+  return true
+end
+
 def no_repeats(year_start, year_end)
+  no_repeat_years = []
+  (year_start..year_end).each do |yr|
+    no_repeat_years << yr if repeat_year?(yr)
+  end
+  no_repeat_years
 end
