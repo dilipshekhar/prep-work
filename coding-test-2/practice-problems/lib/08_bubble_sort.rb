acc5c7ed3c2b326c6361e012aec113101b8ea2d8
dilipshekhar <shekhar.dilip@gmail.com>
2014-05-26 16:14:52 -0400

    Update 08_bubble_sort.rb

--- coding-test-2/practice-problems/lib/08_bubble_sort.rb
+++ coding-test-2/practice-problems/lib/08_bubble_sort.rb
@@ -1,2 +1,13 @@
 def bubble_sort(arr)
+	sorted = false
+	until sorted == true
+		sorted = true
+		(0..(arr.count - 2)).each do |i|
+			if arr[i] > arr[i + 1]
+				arr[i], arr[i + 1] = arr[i + 1], arr[i]
+				sorted = false
+			end
+		end
+	end
+	arr
 end
