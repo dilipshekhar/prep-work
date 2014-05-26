4ba9da0a2354bcd8f17061302fdab686da032825
dilipshekhar <shekhar.dilip@gmail.com>
2014-05-26 15:38:03 -0400

    Update 03_ordered_vowels.rb

--- coding-test-2/practice-problems/lib/03_ordered_vowels.rb
+++ coding-test-2/practice-problems/lib/03_ordered_vowels.rb
@@ -1,2 +1,15 @@
-def ordered_vowel_words(str)
+def ordered_vowel_word(word)
+	vowels = ["a","e","i","o","u"]
+	word = word.split("")
+	vowels_arr = word.select {|i| vowels.include?(i)}
+	(0...(vowels_arr.count-1)).all? do |i|
+		vowels_arr[i] <= vowels_arr[i + 1]
+	end
+
+end
+
+
+def ordered_vowels?(sentence)
+	sentence = sentence.split(" ")
+	ordered_vowels = sentence.select {|word| ordered_vowel_word(word)}
 end
